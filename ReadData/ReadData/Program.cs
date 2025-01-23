3783918ca9032d15d02b8477d3a6680760d5eb12
sourav das <sourav99166@gmail.com>
2025-01-23 18:34:07 +0530

    code push

--- ReadData/ReadData/Program.cs
+++ ReadData/ReadData/Program.cs
@@ -1,10 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Submodule;
+
 class Program
 {
     public static void Main()
     {
-
-        Console.WriteLine("dad");
+        int a = Twosum.AddTwoNumber(1, 2);
+        Console.WriteLine(a);
     }
 }
